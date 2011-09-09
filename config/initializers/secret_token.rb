0825f6e1f7f49f16371643f76c2e719255f0b272
reinprechtsdorf <reinprechtsdorf@hotmail.com>
2011-09-09 22:58:01 +0200

    Prima Stazione

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staticexample::Application.config.secret_token = '7c684b5bd425181aaf02288deef16fae410de19b00eaadd10fe899f1ece561eb58b583f8b679e7221ef3a42b7403437cef4d0fbadda57a8cff9f82be2d4d0023'
